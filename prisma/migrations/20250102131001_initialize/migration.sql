9af758e5fa97b5314f64d9b8e51a99196e4db2f9
samir1maity <s879maity@gmail.com>
2025-01-02 19:02:51 +0530

    successfully connect the db and add vector extension using db pull

---
+++ prisma/migrations/20250102131001_initialize/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,38 @@
-import { pipeline } from '@xenova/transformers';
+import { pipeline } from "@xenova/transformers";
+import "dotenv/config";
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+
+app.use(express.json());
+const prisma = new PrismaClient();
+
+app.post("/", async (req, res) => {
+  const { id, content } = req.body;
+  const temp = await prisma.document.create({
+    data: {
+      id,
+      content,
+    },
+  });
+
+  console.log("temp", temp);
+
+  res.status(201).json({
+    temp,
+  });
+
+});
 
 const generateEmbeddings = await pipeline(
-  'feature-extraction',
-  'Xenova/all-MiniLM-L6-v2'
+  "feature-extraction",
+  "Xenova/all-MiniLM-L6-v2"
 );
 
 function dotProduct(a: number[], b: number[]) {
   if (a.length !== b.length) {
-    throw new Error('Both arguments must have the same length');
+    throw new Error("Both arguments must have the same length");
   }
 
   let result = 0;
@@ -19,17 +44,31 @@ function dotProduct(a: number[], b: number[]) {
   return result;
 }
 
-const output1 = await generateEmbeddings('That is a happy person', {
-  pooling: 'mean',
-  normalize: true,
-});
+const getEmbeddinf = async () => {
+  const output1 = await generateEmbeddings("That is a happy person", {
+    pooling: "mean",
+    normalize: true,
+  });
 
-const output2 = await generateEmbeddings('That is a happy person', {
-  pooling: 'mean',
-  normalize: true,
-});
+  return Array.from(output1.data)
+}
+
+
+
+
+
+
+
+// const output2 = await generateEmbeddings("That is a happy person", {
+//   pooling: "mean",
+//   normalize: true,
+// });
 
 //@ts-ignore
-const similarity = dotProduct(output1.data, output2.data);
+// const similarity = dotProduct(output1.data, output2.data);
 
-console.log(similarity);
+// console.log(similarity);
+
+app.listen(3000, () => {
+  console.log("server started");
+});
